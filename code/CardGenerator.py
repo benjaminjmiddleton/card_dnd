7bca6b999b987e2a6edaa36ab2b9e13d7a8b2c09
Benjamin Middleton <benjaminjmiddleton@yahoo.com>
2020-09-28 23:47:03 -0400

    Initial commit

---
+++ code/CardGenerator.py
@@ -0,0 +1,55 @@
+import CardWriter
+import xlrd
+
+ATTRIBUTES = [
+    "STR",
+    "DEX",
+    "CON",
+    "INT",
+    "WIS",
+    "CHA"
+]
+
+class Card:
+    def __init__(self, name, card_type, spell_lv, dnd_class, attribute, desc):
+        self.name = name
+        self.card_type = card_type
+        self.spell_lv = spell_lv
+        self.dnd_class = dnd_class
+        self.attribute = attribute
+        self.desc = desc
+
+def read_cards(wb_path):
+    wb = xlrd.open_workbook(wb_path)
+    sheet = wb.sheet_by_index(0)
+
+    card_list = []
+    for i in range(1, sheet.nrows):
+        name = sheet.cell_value(i, 0).strip()
+        card_type = sheet.cell_value(i, 1).strip()
+        spell_lv = sheet.cell_value(i, 2).strip()
+        if spell_lv == '-':
+            spell_lv = None
+        dnd_class = sheet.cell_value(i, 3).strip()
+        class_list = dnd_class.split(", ")
+        if dnd_class == "All":
+            class_list = CardWriter.CLASSES
+        attribute = sheet.cell_value(i, 5).strip()
+        attribute_list = attribute.split(", ")
+        if attribute == "All":
+            attribute_list = ATTRIBUTES
+        description = sheet.cell_value(i, 6).strip()
+
+        for cl in class_list:
+            for att in attribute_list:
+                att = att.strip()
+                cl = cl.strip()
+
+                card = Card(name, card_type, spell_lv, cl, att, description)
+                card_list.append(card)
+    return card_list
+
+card_list = read_cards("Cards.xlsx")
+
+CardWriter.generate_cards(card_list, "all_cards")
+CardWriter.generate_card_back()
---
+++ code/CardWriter.py
@@ -0,0 +1,191 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+from shutil import rmtree
+
+W = int(4096 / 10)
+H = int(4096 / 7)
+NATURALS_TO_100 = []
+for i in range(100):
+    NATURALS_TO_100.append(str(i))
+KEYWORDS_ITALIC = [
+    "Exhaust",
+    "If",
+    "Able",
+    "Inflict",
+    "Negate",
+    "Pacify",
+    "Persistent",
+    "Prevent",
+    "Simple",
+    "Unreactable",
+    "Useless"
+]
+for i in range(len(KEYWORDS_ITALIC)):
+    KEYWORDS_ITALIC.append(KEYWORDS_ITALIC[i]+".")
+    KEYWORDS_ITALIC.append(KEYWORDS_ITALIC[i]+",")
+KEYWORDS_BOLD = [
+    "STR",
+    "DEX",
+    "CON",
+    "INT",
+    "WIS",
+    "CHA",
+    "SCM",
+    "X"
+]
+for i in range(len(KEYWORDS_BOLD)):
+    KEYWORDS_BOLD.append(KEYWORDS_BOLD[i]+".")
+    KEYWORDS_BOLD.append(KEYWORDS_BOLD[i]+",")
+CLASSES = [
+    "BBN",
+    "BRD",
+    "CLR",
+    "DRD",
+    "FTR",
+    "MNK",
+    "PAL",
+    "RGR",
+    "ROG",
+    "SOR",
+    "WIZ"
+]
+
+def generate_cards(card_list, folder_name):
+    class_font = ImageFont.truetype("arial.ttf", 36)
+    type_font = ImageFont.truetype("arial.ttf", 32)
+    type_y = 54
+    attr_font = ImageFont.truetype("arial.ttf", 36)
+    desc_font = ImageFont.truetype("arial.ttf", 24)
+    desc_bold = ImageFont.truetype("arialbd.ttf", 24)
+    desc_italic = ImageFont.truetype("ariali.ttf", 24)
+    spell_font = ImageFont.truetype("arial.ttf", 20)
+    
+    rmtree("assets/cards/"+folder_name)
+
+    try:
+        os.mkdir("assets/cards/"+folder_name)
+    except FileExistsError:
+        pass
+
+    for cl in CLASSES:
+        try:
+            os.mkdir("assets/cards/"+folder_name+"/"+cl)
+        except FileExistsError:
+            pass
+
+    for card in card_list:
+        img = Image.new('RGB', (W, H), color='#FFFFFF')
+        canvas = ImageDraw.Draw(img)
+
+        # draw name
+        name_font = ImageFont.truetype("arial.ttf", 48)
+        text_width, text_height = canvas.textsize(card.name, font=name_font)
+        if text_width > W:
+            size = 48
+            while text_width > W:
+                size -= 4
+                name_font = ImageFont.truetype("arial.ttf", size)
+                text_width, text_height = canvas.textsize(card.name, font=name_font)
+            x = W / 2 - text_width / 2
+            y = (48 - size) / 2
+            canvas.text((x, y), card.name, font=name_font, fill='#AAAAAA')
+        else:
+            x = W / 2 - text_width / 2
+            y = 0
+            canvas.text((x, y), card.name, font=name_font, fill='#AAAAAA')
+
+        # draw type
+        text_width, text_height = canvas.textsize(card.card_type, font=type_font)
+        x = W / 2 - text_width / 2
+        y = type_y
+        canvas.text((x, y), card.card_type, font=type_font, fill='#AAAAAA')
+
+        # draw class
+        text_width, text_height = canvas.textsize(card.dnd_class, font=class_font)
+        x = 2
+        y = H - text_height - 2
+        canvas.text((x, y), card.dnd_class, font=class_font, fill='#AAAAAA')
+
+        # draw attribute
+        text_width, text_height = canvas.textsize(card.attribute, font=attr_font)
+        x = W - text_width - 2
+        y = H - text_height - 2
+        canvas.text((x, y), card.attribute, font=attr_font, fill='#AAAAAA')
+
+        # draw description
+        desc_list = card.desc.split(' ')
+        it = iter(desc_list)
+        line_y = 0
+        word = None
+        done = False
+        while desc_list:
+            if word:
+                line = [word]
+            else:
+                line = []
+            while True:
+                try:
+                    word = next(it)
+                except StopIteration:
+                    done = True
+                    break
+                text_width, text_height = canvas.textsize(' '.join(line)+' '+word, font=desc_font)
+                # if we can fit the word on the line
+                if text_width < W - 20:            
+                    line.append(word)
+                # otherwise, start the next line with word
+                else:
+                    break
+            # draw the line
+            text_width, text_height = canvas.textsize(' '.join(line), font=desc_font)
+            x = W / 2 - text_width / 2
+            y = H / 2 + line_y
+            line_y += text_height
+            for word_to_draw in line:
+                if word_to_draw in NATURALS_TO_100 or word_to_draw in KEYWORDS_BOLD:
+                    text_width, text_height = canvas.textsize(word_to_draw+' ', font=desc_bold)
+                    canvas.text((x, y), word_to_draw, font=desc_bold, fill='#AAAAAA')
+                elif word_to_draw in KEYWORDS_ITALIC:
+                    text_width, text_height = canvas.textsize(word_to_draw+' ', font=desc_italic)
+                    canvas.text((x, y), word_to_draw, font=desc_italic, fill='#AAAAAA')
+                else:
+                    text_width, text_height = canvas.textsize(word_to_draw+' ', font=desc_font)
+                    canvas.text((x, y), word_to_draw, font=desc_font, fill='#AAAAAA')
+                x += text_width
+
+            if done:
+                break
+
+        # draw spell level
+        if card.spell_lv:
+            text_width, text_height = canvas.textsize(card.spell_lv, font=spell_font)
+            x = W / 2 - text_width / 2
+            y = H - text_height - 2
+            canvas.text((x, y), card.spell_lv, font=spell_font, fill='#AAAAAA')
+
+        # save image
+        img_path = "assets/cards/"+folder_name+"/"+card.dnd_class+"/"+card.name+".png"
+        cnt = 1
+        while(os.path.exists(img_path)):
+            img_path = "assets/cards/"+folder_name+"/"+card.dnd_class+"/"+card.name+str(cnt)+".png"
+            cnt += 1
+        img.save(img_path)
+
+def generate_card_back():
+    font = ImageFont.truetype("arial.ttf", 72)
+    img = Image.new('RGB', (W, H), color="#AAAAFF")
+    canvas = ImageDraw.Draw(img)
+
+    str_to_draw = "Card"
+    text_width, text_height = canvas.textsize(str_to_draw, font=font)
+    x = W / 2 - text_width / 2
+    y = H / 2 - text_height
+    canvas.text((x, y), str_to_draw, font=font, fill='#AA55FF')
+
+    str_to_draw = "DnD"
+    text_width, text_height = canvas.textsize(str_to_draw, font=font)
+    x = W / 2 - text_width / 2
+    y = H / 2
+    canvas.text((x, y), str_to_draw, font=font, fill='#AA55FF')
+
+    img.save("assets/cards/back.png")
