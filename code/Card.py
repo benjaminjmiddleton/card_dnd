2993ed3bdcb7c1bfcd42b8dc9bcd0bd7af6bdfd7
Benjamin Middleton <benjaminjmiddleton@yahoo.com>
2021-04-04 02:59:26 -0400

    MonsterManager.py inital commit

---
+++ code/Card.py
@@ -0,0 +1,53 @@
+# Card.py
+#
+# Contains shared classes and methods for import.
+
+import xlrd
+
+ATTRIBUTES = [
+    "STR",
+    "DEX",
+    "CON",
+    "INT",
+    "WIS",
+    "CHA"
+]
+
+class Card:
+    def __init__(self, name, card_type, pack, dnd_class, attribute, desc):
+        self.name = name
+        self.card_type = card_type
+        self.pack = pack
+        self.dnd_class = dnd_class
+        self.attribute = attribute
+        self.desc = desc
+
+def read_cards(wb_path):
+    wb = xlrd.open_workbook(wb_path)
+    sheet = wb.sheet_by_index(0)
+
+    card_list = []
+    for i in range(1, sheet.nrows):
+        name = sheet.cell_value(i, 0).strip()
+        card_type = sheet.cell_value(i, 1).strip()
+        pack = str(sheet.cell_value(i, 2)).strip()
+        if pack == '-':
+            pack = None
+        dnd_class = sheet.cell_value(i, 3).strip()
+        class_list = dnd_class.split(", ")
+        # if dnd_class == "All":
+        #     class_list = CardWriter.CLASSES
+        attribute = sheet.cell_value(i, 4).strip()
+        attribute_list = attribute.split(", ")
+        if attribute == "All":
+            attribute_list = ATTRIBUTES
+        description = sheet.cell_value(i, 5).strip()
+
+        for cl in class_list:
+            for att in attribute_list:
+                att = att.strip()
+                cl = cl.strip()
+
+                card = Card(name, card_type, pack, cl, att, description)
+                card_list.append(card)
+    return card_list
---
+++ code/MonsterManager.py
@@ -0,0 +1,101 @@
+# MonsterManager.py
+#
+# This script should allow you to run many enemies more effectively in card d&d than you would in TTS.
+#
+
+import xlrd
+
+from Card import Card, read_cards
+
+class Monster:
+    def __init__(self, name, hp, _str, dex, con, _int, wis, cha, deck):
+        self.name = name
+        self.hp = hp
+        self.str = _str
+        self.dex = dex
+        self.con = con
+        self.int = _int
+        self.wis = wis
+        self.cha = cha
+        self.deck = deck
+
+def get_monsters(monster_manual, valid_monster_names, card_list):
+    monster_list = []
+
+    print('Adding monsters to encounter.')
+    adding_monsters = True
+    while adding_monsters:
+        monster_name = input('Monster to add: ')
+        if monster_name in valid_monster_names:
+            monster_list.append(monster_name)
+        else:
+            print("Not a valid monster name!")
+        another = input("Add another monster (y)/n: ")
+        if 'n' in another or 'N' in another:
+            adding_monsters = False
+
+    for monster_it in range(len(monster_list)):
+        monster = monster_list[monster_it]
+        sheet = monster_manual.sheet_by_name(monster)
+
+        deck = []
+        hp = _str = dex = con = _int = wis = cha = None
+
+        name = sheet.cell_value(0, 0)
+        for i in range(50):
+            for j in range(10):
+                try:
+                    if sheet.cell_value(i, j) == 'Hit Points':
+                        hp = sheet.cell_value(i, j+1)
+                    if sheet.cell_value(i, j) == 'STR':
+                        _str = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'DEX':
+                        dex = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'CON':
+                        con = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'INT':
+                        _int = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'WIS':
+                        wis = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'CHA':
+                        cha = sheet.cell_value(i+1, j)
+                    if sheet.cell_value(i, j) == 'Example Deck':
+                        row = i
+                        cell_value = sheet.cell_value(row+1, j)
+                        while cell_value:
+                            x_ind = cell_value.rfind('x')
+                            copies = int(cell_value[x_ind+1:])
+                            card_name = cell_value[:x_ind-1]
+                            my_card = None
+                            for card in card_list:
+                                if card.name == card_name:
+                                    my_card = card
+                                    break
+                            if not my_card:
+                                raise CardNotFound
+                            count = 0
+                            while count < copies:
+                                deck.append(my_card)
+                                count += 1
+                            row += 1
+                            cell_value = sheet.cell_value(row+1, j)
+                except IndexError:
+                    pass
+        monster_list[monster_it] = Monster(name, hp, _str, dex, con, _int, wis, cha, deck)
+    return monster_list
+
+def main():
+    monster_manual = xlrd.open_workbook("MonsterManual.xlsx")
+    monster_names = monster_manual.sheet_names()
+    card_list = read_cards("Cards.xlsx")
+    encounter_monsters = get_monsters(monster_manual, monster_names, card_list)
+
+    # for monster in encounter_monsters:
+    #     print(monster.__dict__)
+    #     for card in monster.deck:
+    #         print(card.__dict__)
+
+    # monster_manual.sheet_by_name()
+    
+if __name__ == "__main__":
+    main()
